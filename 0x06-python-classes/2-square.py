e5dafba63a8a0133a3ce3b77a28338d9a971103a
root <root@kali>
2021-12-13 10:36:22 +0300

    Updating : 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+Creating a class square
+"""
+
+
 class Square:
     """ A class that defines a square by its size
     """
+
     def __init__(self, size=0):
         """ Method to initialize the square object
         """
