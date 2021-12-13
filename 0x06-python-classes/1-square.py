aca6dc8fef9adc6f2f1eda9339043f4bacd28a60
root <root@kali>
2021-12-13 10:29:32 +0300

    Updating : 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+Creating class square with private attribute size
+"""
+
+
 class Square:
     """Class Square that defines a square object
     """
+
     def __init__(self, size):
         """Initialize method that stores the size of the square
 
