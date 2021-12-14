0ca03f59544a676588a88abb2e6ef8c362334cfb
root <root@kali>
2021-12-14 11:06:19 +0300

    [Adding] : 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Function that prints Your first and last name
+"""
+def say_my_name(first_name, last_name=""):
+    """
+    Function tht prints first and last name in string format
+    """
+    if type(first_name) not in [str]:
+        raise TypeError("first_name must be a string")
+    if type(last_name) not in [str]:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name,last_name))
