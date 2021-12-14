1b64fd285d58561d13085e6386abe45aeb27202d
root <root@kali>
2021-12-14 08:04:21 +0300

    [Adding] : 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Function that adds two integers
+"""
+
+def add_integer(a, b=98):
+    """
+    Function that adds two integers a,b
+    """
+    if type(a) not in [int,float]:
+        raise TypeError("a must be an integer")
+    elif type(b) not in [int,float]:
+        raise TypeError("b must be an integer")
+    elif type(a) in [float]:
+        int(a)
+    elif type(b) in [float]:
+        int(b)
+
+    return int(a + b)
