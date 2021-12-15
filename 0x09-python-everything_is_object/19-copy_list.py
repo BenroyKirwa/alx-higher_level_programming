97efe5c94f201002b114b2488608804ef45d423b
root <root@kali>
2021-12-15 09:05:34 +0300

    [Adding] : 19-copy_list.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
-"""
-Function that returns a copy of a list
-"""
-
 def copy_list(l):
-    """
-    Function that returns a copy of a list
-    """
-
     new_l = l.copy()
     return new_l
