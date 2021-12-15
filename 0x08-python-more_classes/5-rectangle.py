be16f3dfa7e253b0ef310e03d335dfc20eefbc7f
root <root@kali>
2021-12-15 11:11:35 +0300

    [Update] : 5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -9,25 +9,25 @@ class Rectangle:
     class Rectangle that defines a rectangle by width and height
     """
 
-    def width(self,value):
+    def width(self, value):
         """
         Attribute to set the width of a rectangle
         """
-        if not isinstance(value,int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >=0")
 
-    def height(self,value):
+    def height(self, value):
         """
         Attribute to set the length of a rectangle
         """
-        if not isinstance(value,int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >=0")
 
-    def __init__(self,width=0,height=0):
+    def __init__(self, width=0, height=0):
         """
         Instantiation with optional width and height
         """
@@ -62,14 +62,14 @@ class Rectangle:
         elif self.height == 0:
             print()
         else:
-            for i in range(0,self.height):
+            for i in range(0, self.height):
                 for k in range(self.width):
-                    print("#",end='')
+                    print("#", end='')
                 print()
             return str()
 
     def __repr__(self):
-        return "Rectangle({:d},{:d})".format(self.width,self.height)
+        return "Rectangle({:d},{:d})".format(self.width, self.height)
 
     def __del__(self):
         print("Bye rectangle...")
