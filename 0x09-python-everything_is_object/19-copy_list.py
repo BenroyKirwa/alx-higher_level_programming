412261c79f1812a2ac595e27040333a62d004f19
root <root@kali>
2021-12-15 09:01:16 +0300

    [Adding] : 19-copy_list.py

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Function that returns a copy of a list
+"""
+
+def copy_list(l):
+    """
+    Function that returns a copy of a list
+    """
+
+    new_l = l.copy()
+    return new_l
