7fe892e08dfea4e77269e217b90bc51158a09ae5
root <root@kali>
2021-12-15 12:02:44 +0300

    [Update] : 4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -9,25 +9,25 @@ class Rectangle:
     class Rectangle that defines a rectangle by width and height
     """
 
-    def width(self,value):
+    def width(self, value):
         """
         Attribute to set the width of a rectangle
         """
-        if not isinstance(value,int):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >=0")
 
-    def height(self,value):
+    def height(self, value):
         """
         Attribute to set the length of a rectangle
         """
-        if not isinstance(value,int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >=0")
 
-    def __init__(self,width=0,height=0):
+    def __init__(self, width=0, height=0):
         """
         Instantiation with optional width and height
         """
@@ -57,16 +57,15 @@ class Rectangle:
         """
         Prints the rectangle with the character #
         """
+        spc = ""
         if self.width == 0:
-            print()
+            return spc
         elif self.height == 0:
-            print()
+            return spc
         else:
-            for i in range(0,self.height):
-                for k in range(self.width):
-                    print("#",end='')
-                print()
-            return str()
+            for i in range(0, self.height):
+                spc += ("#"*self.width) + "\n"
+            return str(spc[:-1])
 
     def __repr__(self):
         return "Rectangle({:d},{:d})".format(self.width,self.height)
