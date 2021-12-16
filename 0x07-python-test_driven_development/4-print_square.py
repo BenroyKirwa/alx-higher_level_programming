c7624b228c14b424c6b1c13a6726c63c5d5e3f5a
root <root@kali>
2021-12-16 09:21:08 +0300

    [Update] : 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -2,12 +2,16 @@
 """
 Function that prints a square with the character #
 """
+
+
 def print_square(size):
-    for i in range(0,size):
-        print("#"*size)
+
+
     if type(size) not in [int]:
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
     elif size < 0 and type(size) in [float]:
         raise TypeError("size must be an integer")
+    for i in range(0,size):
+        print ("#"*size)
