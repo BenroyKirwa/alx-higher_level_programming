0b549ff8f1164e8d23b28a7c1c24ffd723f32b23
root <root@kali>
2021-12-21 20:13:22 +0300

    [Update] : files

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+""" Function that returns True/False if obj is a type of a_class
+
+    Args:
+        obj: object
+        a_class: class type
+
+    Returns:
+        True if type of obj is a_class
+        False, otherwise
+"""
+
+
 def is_same_class(obj, a_class):
     """ Function that returns True/False if obj is a type of a_class
 
@@ -10,4 +22,5 @@ def is_same_class(obj, a_class):
         True if type of obj is a_class
         False, otherwise
     """
+
     return type(obj) is a_class
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+""" Function that returns True/False if obj is an instance of a_class
+
+    Args:
+        obj: object
+        a_class: class type
+
+    Returns:
+        True if obj is an instance of a_class
+        False, otherwise
+"""
+
+
 def inherits_from(obj, a_class):
     """ Function that returns True/False if obj is an instance of a_class
 
@@ -10,6 +22,7 @@ def inherits_from(obj, a_class):
         True if obj is an instance of a_class
         False, otherwise
     """
+
     if type(obj) is a_class:
         return False
     return isinstance(obj, a_class)
