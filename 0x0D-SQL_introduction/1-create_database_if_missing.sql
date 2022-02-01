cbeeffe32dd9f8c2f5f5330f078d8fe23fa79c93
root <root@kali>
2022-02-01 21:55:19 +0300

    [Update] : Comments

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
---Creates the database hbtn_0c_0
---If it already exists,it doesn't fail
+-- Creates the database hbtn_0c_0
+-- If it already exists,it doesn't fail
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
