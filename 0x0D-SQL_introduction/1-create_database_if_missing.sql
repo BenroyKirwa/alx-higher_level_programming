0fc7323a54724a0986ec5f2f23b434d759d93c0d
root <root@kali>
2022-02-01 21:51:59 +0300

    [Update] : Adding control flow

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,10 +1,3 @@
 --Creates the database hbtn_0c_0
 --If it already exists,it doesn't fail
-IF (EXISTS)
-BEGIN
-	print 'Database already exists'
-END;
-ELSE
-BEGIN
-	CREATE DATABASE hbtn_0c_0;
-END;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
