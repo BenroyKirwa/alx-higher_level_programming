2e5803764aa227a843a470bd73f7e63930785f9f
root <root@kali>
2022-02-03 11:13:55 +0300

    [Update] : Converting Database

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,5 @@
 -- Converts the database to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
 ALTER TABLE first_table MODIFY COLUMN name
 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
